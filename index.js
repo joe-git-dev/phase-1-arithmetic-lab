8b7ae37372510983ba6ede36ddc279ec9cd300fa
joe welch <jwelch19@outlook.com>
2022-03-01 23:00:33 -0500

    commit tree 182e70053e3cada8f4e42c19b340636f617dcc88

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+
+let multiply = num1 * num2;
+
+multiply;
+
+let random = Math.ceil(Math.random()*10);
+
+random;
+
+let num3 = 14;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+mod;
+
+let max = Math.max(1, 3, 20);
+
+max;
